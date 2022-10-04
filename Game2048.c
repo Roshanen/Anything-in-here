0870497ddaaa7e37301b4f09c793400c2ade1005
Roshanen <106764256+Roshanen@users.noreply.github.com>
2022-10-04 22:03:37 +0700

    here we go again!

--- Game2048.c
+++ Game2048.c
@@ -1,242 +1,287 @@
 #include <stdio.h>
-#include <ctype.h>
 #include <time.h>
 #include <stdlib.h>
-
-int main()
+int table[6][6] = {0};
+int tube[6][6];
+int win = 0;
+int x = 0, y = 0;
+char kb = ' ';
+void set_table()
 {
-    int x = rand() % 4 + 1, y = rand() % 4 + 1; // initial random two 2
-    srand(time(NULL));
-    int table[6][6] = {0};
-    table[x][y] = 2;
-    table[(x + rand()) % 4 + 1][(y + rand()) % 4 + 1] = 2;
-    int tube[6][6];
-    int win = 0;
-    int movable = 1;
-    while (movable == 1 && win == 0)
+    for (int i = 1; i < 5; i++)
     {
-        for (int i = 1; i < 5; i++) // printf
+        for (int j = 1; j < 5; j++)
+            table[i][j] = 0;
+    }
+    table[rand() % 4 + 1][rand() % 4 + 1] = 2;
+    table[rand() % 4 + 1][rand() % 4 + 1] = 2;
+}
+void print_table()
+{
+    for (int i = 1; i < 5; i++) // printf
+    {
+        for (int j = 1; j < 5; j++)
         {
-            for (int j = 1; j < 5; j++)
+            int digit = table[i][j];
+            for (int k = 1; k < 5; k++)
             {
-                int bottle = table[i][j];
-                for (int k = 1; k < 5; k++)
-                {
-                    if (bottle / 10 == 0)
-                        printf(" ");
-                    bottle /= 10;
-                }
-                printf("%d ", table[i][j]);
-                tube[i][j] = table[i][j];
+                if (digit / 10 == 0)
+                    printf(" ");
+                digit /= 10;
             }
-            printf("\n");
+            printf("%d ", table[i][j]);
+            tube[i][j] = table[i][j];
         }
-
-        char kb = ' '; // input
-        printf("\ninput (U,D,L,R,F to exit) : ");
-        scanf(" %c", &kb);
         printf("\n");
-        kb = toupper(kb);
-
-        if (kb == 'F')
+    }
+}
+int movable()
+{
+    for (int i = 1; i < 5; i++) // check movable
+    {
+        for (int j = 1; j < 5; j++)
         {
-            printf("Bye bye.");
-            return 0;
-        }
-        else if (kb != 'U' && kb != 'D' && kb != 'L' && kb != 'R')
-        {
-            printf("invalid input!\n");
-            continue;
-        }
-        movable = 0;
-        for (int i = 1; i < 5; i++) // check movable
-        {
-            for (int j = 1; j < 5; j++)
-            {
-                if ((table[i][j] == 0) ||
-                    (table[i + 1][j] == table[i][j]) ||
-                    (table[i - 1][j] == table[i][j]) ||
-                    (table[i][j + 1] == table[i][j]) ||
-                    (table[i][j - 1] == table[i][j]))
-                {
-                    movable = 1;
-                    break;
-                }
-            }
-        }
-        if (kb == 'U') // Up
-        {
-            for (int col = 1; col < 5; col++)
-            {
-                for (int cur = 1; cur < 5; cur++)
-                {
-                    if (table[cur][col] == 0)
-                    {
-                        for (int i = cur + 1; i < 5; i++)
-                        {
-                            if (table[i][col] != 0)
-                            {
-                                table[cur][col] = table[i][col];
-                                table[i][col] = 0;
-                                break;
-                            }
-                        }
-                    }
-                    if (table[cur][col] != 0)
-                    {
-                        for (int i = cur + 1; i < 5; i++)
-                        {
-                            if (table[i][col] == table[cur][col])
-                            {
-                                table[cur][col] *= 2;
-                                win = (table[cur][col] == 2048) ? 1 : 0;
-                                table[i][col] = 0;
-                                break;
-                            }
-                            else if (table[i][col] != table[cur][col] && table[i][col] != 0)
-                                break;
-                        }
-                    }
-                }
-            }
-        }
-        else if (kb == 'D') // Down
-        {
-            for (int col = 1; col < 5; col++)
-            {
-                for (int cur = 4; cur >= 1; cur--)
-                {
-                    if (table[cur][col] == 0)
-                    {
-                        for (int i = cur - 1; i >= 1; i--)
-                        {
-                            if (table[i][col] != 0)
-                            {
-                                table[cur][col] = table[i][col];
-                                table[i][col] = 0;
-                                break;
-                            }
-                        }
-                    }
-                    if (table[cur][col] != 0)
-                    {
-                        for (int i = cur - 1; i >= 1; i--)
-                        {
-                            if (table[i][col] == table[cur][col])
-                            {
-                                table[cur][col] *= 2;
-                                win = (table[cur][col] == 2048) ? 1 : 0;
-                                table[i][col] = 0;
-                                break;
-                            }
-                            else if (table[i][col] != table[cur][col] && table[i][col] != 0)
-                                break;
-                        }
-                    }
-                }
-            }
-        }
-        else if (kb == 'L') // Left
-        {
-            for (int col = 1; col < 5; col++)
-            {
-                for (int cur = 1; cur < 5; cur++)
-                {
-                    if (table[col][cur] == 0)
-                    {
-                        for (int i = cur + 1; i < 5; i++)
-                        {
-                            if (table[col][i] != 0)
-                            {
-                                table[col][cur] = table[col][i];
-                                table[col][i] = 0;
-                                break;
-                            }
-                        }
-                    }
-                    if (table[col][cur] != 0)
-                    {
-                        for (int i = cur + 1; i < 5; i++)
-                        {
-                            if (table[col][i] == table[col][cur])
-                            {
-                                table[col][cur] *= 2;
-                                win = (table[col][cur] == 2048) ? 1 : 0;
-                                table[col][i] = 0;
-                                break;
-                            }
-                            else if (table[col][i] != table[col][cur] && table[col][i] != 0)
-                                break;
-                        }
-                    }
-                }
-            }
-        }
-        else if (kb == 'R') // Right
-        {
-            for (int col = 1; col < 5; col++)
-            {
-                for (int cur = 4; cur >= 1; cur--)
-                {
-                    if (table[col][cur] == 0)
-                    {
-                        for (int i = cur - 1; i >= 1; i--)
-                        {
-                            if (table[col][i] != 0)
-                            {
-                                table[col][cur] = table[col][i];
-                                table[col][i] = 0;
-                                break;
-                            }
-                        }
-                    }
-                    if (table[col][cur] != 0)
-                    {
-                        for (int i = cur - 1; i >= 1; i--)
-                        {
-                            if (table[col][i] == table[col][cur])
-                            {
-                                table[col][cur] *= 2;
-                                win = (table[col][cur] == 2048) ? 1 : 0;
-                                table[col][i] = 0;
-                                break;
-                            }
-                            else if (table[col][i] != table[col][cur] && table[col][i] != 0)
-                                break;
-                        }
-                    }
-                }
-            }
-        }
-        int same = 1;
-        for (int i = 1; i < 5; i++)
-        {
-            for (int j = 1; j < 5; j++)
-            {
-                if (tube[i][j] != table[i][j])
-                {
-                    same = 0;
-                    break;
-                }
-            }
-        }
-        if (same == 0)
-        {
-            x = rand() % 4 + 1;
-            y = rand() % 4 + 1;
-            while (table[x][y] != 0) // rand2
-            {
-                x = rand() % 4 + 1;
-                y = rand() % 4 + 1;
-                if (table[x][y] == 0)
-                    break;
-            }
-            table[x][y] = 2;
+            if ((table[i + 1][j] == table[i][j]) ||
+                (table[i - 1][j] == table[i][j]) ||
+                (table[i][j + 1] == table[i][j]) ||
+                (table[i][j - 1] == table[i][j]))
+                return 1;
         }
     }
-    if (win == 1)
-        printf("You win 2048 Game.");
-    else
-        printf("Nice try.");
+    return 0;
+}
+int zero()
+{
+    for (int i = 1; i < 5; i++) // check movable
+    {
+        for (int j = 1; j < 5; j++)
+        {
+            if (table[i][j] == 0)
+                return 1;
+        }
+    }
+    return 0;
+}
+int same()
+{
+    for (int i = 1; i < 5; i++)
+    {
+        for (int j = 1; j < 5; j++)
+        {
+            if (tube[i][j] != table[i][j])
+                return 0;
+        }
+    }
+    return 1;
+}
+void random2()
+{
+    x = rand() % 4 + 1;
+    y = rand() % 4 + 1;
+    while (table[x][y] != 0) // rand2
+    {
+        x = rand() % 4 + 1;
+        y = rand() % 4 + 1;
+        if (table[x][y] == 0)
+            break;
+    }
+    table[x][y] = (rand() % 100 >= 20) ? 2 : 4;
+}
+int main()
+{
+    srand(time(NULL));
+    while (1)
+    {
+        set_table();
+        win = 0;
+        while (movable() == 1 && win == 0)
+        {
+            print_table();
+            kb = ' ';
+            printf("\ninput ((W)U, (X)D, (A)L, (D)R, F to exit) : ");
+            scanf(" %c", &kb);
+            fflush(stdin);
+            printf("\n");
+            if (kb == 'F' || kb == 'f')
+            {
+                printf("Bye bye.");
+                return 0;
+            }
+            else if (kb != 'A' && kb != 'W' && kb != 'S' && kb != 'D' &&
+                     kb != 'a' && kb != 'w' && kb != 's' && kb != 'd')
+            {
+                printf("invalid input!\n");
+                continue;
+            }
+            if (kb == 'W' || kb == 'w') // Up
+            {
+                for (int col = 1; col < 5; col++) // k
+                {
+                    for (int cur = 1; cur < 5; cur++) // cur =
+                    {
+                        if (table[cur][col] == 0) // 1 2 3 4
+                        {
+                            for (int i = cur + 1; i < 5; i++)
+                            {
+                                if (table[i][col] != 0)
+                                {
+                                    table[cur][col] = table[i][col];
+                                    table[i][col] = 0;
+                                    break;
+                                }
+                            }
+                        }
+                        if (table[cur][col] != 0)
+                        {
+                            for (int i = cur + 1; i < 5; i++)
+                            {
+                                if (table[i][col] == table[cur][col])
+                                {
+                                    table[cur][col] *= 2;
+                                    win = (table[cur][col] == 2048) ? 1 : 0;
+                                    table[i][col] = 0;
+                                    break;
+                                }
+                                else if (table[i][col] != table[cur][col] && table[i][col] != 0)
+                                    break;
+                            }
+                        }
+                    }
+                }
+            }
+            else if (kb == 'S' || kb == 's') // Down
+            {
+                for (int col = 1; col < 5; col++)
+                {
+                    for (int cur = 4; cur >= 1; cur--)
+                    {
+                        if (table[cur][col] == 0)
+                        {
+                            for (int i = cur - 1; i >= 1; i--)
+                            {
+                                if (table[i][col] != 0)
+                                {
+                                    table[cur][col] = table[i][col];
+                                    table[i][col] = 0;
+                                    break;
+                                }
+                            }
+                        }
+                        if (table[cur][col] != 0)
+                        {
+                            for (int i = cur - 1; i >= 1; i--)
+                            {
+                                if (table[i][col] == table[cur][col])
+                                {
+                                    table[cur][col] *= 2;
+                                    win = (table[cur][col] == 2048) ? 1 : 0;
+                                    table[i][col] = 0;
+                                    break;
+                                }
+                                else if (table[i][col] != table[cur][col] && table[i][col] != 0)
+                                    break;
+                            }
+                        }
+                    }
+                }
+            }
+            else if (kb == 'A' || kb == 'a') // Left
+            {
+                for (int col = 1; col < 5; col++)
+                {
+                    for (int cur = 1; cur < 5; cur++)
+                    {
+                        if (table[col][cur] == 0)
+                        {
+                            for (int i = cur + 1; i < 5; i++)
+                            {
+                                if (table[col][i] != 0)
+                                {
+                                    table[col][cur] = table[col][i];
+                                    table[col][i] = 0;
+                                    break;
+                                }
+                            }
+                        }
+                        if (table[col][cur] != 0)
+                        {
+                            for (int i = cur + 1; i < 5; i++)
+                            {
+                                if (table[col][i] == table[col][cur])
+                                {
+                                    table[col][cur] *= 2;
+                                    win = (table[col][cur] == 2048) ? 1 : 0;
+                                    table[col][i] = 0;
+                                    break;
+                                }
+                                else if (table[col][i] != table[col][cur] && table[col][i] != 0)
+                                    break;
+                            }
+                        }
+                    }
+                }
+            }
+            else if (kb == 'D' || kb == 'd') // Right
+            {
+                for (int col = 1; col < 5; col++)
+                {
+                    for (int cur = 4; cur >= 1; cur--)
+                    {
+                        if (table[col][cur] == 0)
+                        {
+                            for (int i = cur - 1; i >= 1; i--)
+                            {
+                                if (table[col][i] != 0)
+                                {
+                                    table[col][cur] = table[col][i];
+                                    table[col][i] = 0;
+                                    break;
+                                }
+                            }
+                        }
+                        if (table[col][cur] != 0)
+                        {
+                            for (int i = cur - 1; i >= 1; i--)
+                            {
+                                if (table[col][i] == table[col][cur])
+                                {
+                                    table[col][cur] *= 2;
+                                    win = (table[col][cur] == 2048) ? 1 : 0;
+                                    table[col][i] = 0;
+                                    break;
+                                }
+                                else if (table[col][i] != table[col][cur] && table[col][i] != 0)
+                                    break;
+                            }
+                        }
+                    }
+                }
+            }
+            if (zero() == 1)
+                random2();
+        }
+        if (win == 1)
+        {
+            print_table();
+            printf("You win 2048 Game.\n");
+            printf("Again (Y/else)? : ");
+            scanf("%c", &kb);
+            if (kb == 'Y' || kb == 'y')
+                continue;
+            else
+                break;
+        }
+        else if (movable() == 0)
+        {
+            print_table();
+            printf("Nice try.\n");
+            printf("Again (Y/N)? : ");
+            scanf("%c", &kb);
+            if (kb == 'Y' || kb == 'y')
+                continue;
+            else
+                break;
+        }
+    }
     return 0;
 }
